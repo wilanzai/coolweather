11b1048514ff3d67829e11e4100d5f08acc77102
wilanzai <547681579@qq.com>
2016-12-26 13:39:20 +0800

    add

--- app/src/main/java/com/example/wilanzai/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/wilanzai/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.wilanzai.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.wilanzai.coolweather.gson.Forecast;
 import com.example.wilanzai.coolweather.gson.Weather;
+import com.example.wilanzai.coolweather.service.AutoUpdateService;
 import com.example.wilanzai.coolweather.util.HttpUtil;
 import com.example.wilanzai.coolweather.util.Utility;
 
@@ -240,6 +242,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
